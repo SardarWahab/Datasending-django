62d47b1a1c7e1da6d36250af03b637e9035e8d24
SardarWahab <wahabsardar130@gmail.com>
2024-08-31 05:45:41 +0500

    secondcommit

--- datasending/views.py
+++ datasending/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-def home(request,user,user1,user2):
-    context = {
-        'name':user,
-        'Roll':user1,
-        'age':user2
-    }
+def home(request,):
+    # context = {
+    #     'name':user,
+    #     'Roll':user1,
+    #     'age':user2
+    # }
     
     
-    return render(request, 'home.html', context)
+    return render(request, 'home.html')
